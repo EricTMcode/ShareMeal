3b90c641bfa34003a030fab207f1531111050eb6
Eric <ebagnaro@gmail.com>
2023-10-23 14:11:16 +0200

    Initial Commit

---
+++ ShareMeal/ShareMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareMealApp.swift
+//  ShareMeal
+//
+//  Created by Eric on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShareMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
